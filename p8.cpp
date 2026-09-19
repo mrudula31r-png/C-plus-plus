643441d585531c857be1488ddb1ef25401d456ef
Mrudula_R <mrudula31r@gmail.com>
2026-09-19 21:50:46 +0530

    Update p8.cpp

--- p8.cpp
+++ p8.cpp
@@ -1,1 +1,18 @@
+// Program to find even or odd number
+#include <iostream>
 
+int main() {
+    int number;
+
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    // A number is even if it is divisible by 2 with no remainder
+    if (number % 2 == 0) {
+        std::cout << number << " is even." << std::endl;
+    } else {
+        std::cout << number << " is odd." << std::endl;
+    }
+
+    return 0;
+}
